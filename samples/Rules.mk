caf89aaa0e425b64bda5ac54403e63536f7a10f2
Bill <bill@yuan.com.tw>
2024-04-10 15:50:05 +0800

    Fixed cross compile issues for xlnk_arm64

--- samples/Rules.mk
+++ samples/Rules.mk
@@ -1,42 +1,29 @@
-# Clear the flags from env
-CPPFLAGS :=
-LDFLAGS :=
-
 # Verbose flag
-ifeq ($(VERBOSE), 1)
+ifeq ($(V), 1)
 AT =
 else
 AT = @
 endif
 
-# TARGET_ROOTFS ?= /usr/targets/aarch64-unknown/
-# CROSS_COMPILE ?= aarch64-unknown-linux-gnu-
-
-AS             = $(AT) $(CROSS_COMPILE)as
-LD             = $(AT) $(CROSS_COMPILE)ld
-CC             = $(AT) $(CROSS_COMPILE)gcc
-CPP            = $(AT) $(CROSS_COMPILE)g++
-AR             = $(AT) $(CROSS_COMPILE)ar
-NM             = $(AT) $(CROSS_COMPILE)nm
-STRIP          = $(AT) $(CROSS_COMPILE)strip
-OBJCOPY        = $(AT) $(CROSS_COMPILE)objcopy
-OBJDUMP        = $(AT) $(CROSS_COMPILE)objdump
-
-# Specify the logical root directory for headers and libraries.
-ifneq ($(TARGET_ROOTFS),)
-CPPFLAGS += --sysroot=$(TARGET_ROOTFS)
-CFLAGS += --sysroot=$(TARGET_ROOTFS)
-LDFLAGS +=
-endif
+AS             ?= as
+LD             ?= ld
+CC             ?= gcc
+CXX            ?= g++
+AR             ?= ar
+NM             ?= nm
+STRIP          ?= strip
+OBJCOPY        ?= objcopy
+OBJDUMP        ?= objdump
 
 # All common header files
-CPPFLAGS += -std=c++11 \
+CXXFLAGS += -std=c++11 \
 -I../common
 CFLAGS += \
 -I../common
 
 # All common dependent libraries
 LDFLAGS += \
-	-pthread
+-pthread
 
 COMMON_DIR := ../common
+
